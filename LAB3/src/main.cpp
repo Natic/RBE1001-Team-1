59dd82f49eb665c79763bca98e4f8bfe97fb77f6
Yusuf Simsek <yusuftariksimsek@gmail.com>
2022-11-22 18:05:09 -0500

    Fixed the movement
    someone set the horizontal error to be proportional to Y AND NOT X, AND IT TOOK ME AN HOUR TO FIND

--- LAB3/src/main.cpp
+++ LAB3/src/main.cpp
@@ -16,21 +16,22 @@
 // ---- END VEXCODE CONFIGURED DEVICES ----
 
 const int TARGET_POS = 150;
-const int TARGET_WIDTH = 60;
+const int TARGET_WIDTH = 30;
 
 bool moving = false;
 
 void turnDrive(int turn, int move)
 {
-  int leftDrive = turn + move;
+  // POSITIVE TURN MEANS TURN RIGHT.
+  int leftDrive  = move + turn;
   int rightDrive = move - turn;
 
-  if (turn != 0)
+  if (turn != 0 || move != 0)
     moving = true;
   else
     moving = false;
-  leftDriveMotor.spin(fwd, leftDrive, pct);
-  rightDriveMotor.spin(fwd, rightDrive, pct);
+  leftDriveMotor.spin(reverse, leftDrive, pct);
+  rightDriveMotor.spin(reverse, rightDrive, pct);
 }
 
 void visionControl()
@@ -45,41 +46,63 @@ void visionControl()
 
       vex::vision::object obj = Vision.objects[0];
 
-      if (obj.centerY != 0 && obj.width > 20 && obj.height > 20)
+      if (obj.centerY != 0 && obj.width > 10)
       {
-        int side_error = obj.centerY - TARGET_POS;
-        int front_error = obj.width - TARGET_WIDTH;
+        int side_error = obj.centerX - TARGET_POS; // THE MORE RIGHT, THE MORE ERROR
+        Brain.Screen.setCursor(1, 10);
+        Brain.Screen.print(obj.centerX);
+        Brain.Screen.setCursor(1, 20);
+        Brain.Screen.print(side_error);
+        float front_error = obj.width - TARGET_WIDTH; // THE CLOSER, THE MORE ERROR
+        Brain.Screen.setCursor(2, 10);
+        Brain.Screen.print(obj.width);
+        Brain.Screen.setCursor(2, 20);
+        Brain.Screen.print(front_error);
 
-        int turn_spd = side_error;
-        if (turn_spd > 10)
-          turn_spd = 10;
-        if (turn_spd < -10)
-          turn_spd = -10;
+        int turn_spd = side_error; // THE MORE ERROR, THE MORE TURN RIGHT
+        if (turn_spd > 20)
+          turn_spd = 20;
+        if (turn_spd < -20)
+          turn_spd = -20;
 
-        if (obj.centerY > (TARGET_POS - 5) && obj.centerY < (TARGET_POS + 5) && obj.width > (TARGET_WIDTH - 5) && obj.width < (TARGET_WIDTH + 5))
+        bool horizontally_aligned = obj.centerY > (TARGET_POS - 5) && obj.centerY < (TARGET_POS + 5);
+        bool distance_aligned = obj.width > (TARGET_WIDTH - 5) && obj.width < (TARGET_WIDTH + 5);
+
+        int turn_value;
+        int move_value;
+        
+        if (!horizontally_aligned)
         {
-          turnDrive(0, 0);
+          turn_value = turn_spd / 3;
+          Brain.Screen.setCursor(1, 1);
+          Brain.Screen.print("|!HO|");
+        }else{
+          turn_value = 0;
+          Brain.Screen.setCursor(1, 1);
+          Brain.Screen.print("|HO|");
         }
-        else if (obj.centerY > (TARGET_POS - 15) && obj.centerY < (TARGET_POS + 15))
+        if (!distance_aligned)
         {
-          turnDrive(0,-front_error / 4);
+          move_value = front_error * 2; // THE MORE ERROR, THE MORE MOVE FORWARD
+          Brain.Screen.setCursor(2, 1);
+          Brain.Screen.print("|!DA|");
+        }else{
+          move_value = 0;
+          Brain.Screen.setCursor(2, 1);
+          Brain.Screen.print("|DA|");
         }
-        else if (obj.width > (TARGET_WIDTH - 10) && obj.width < (TARGET_WIDTH + 10))
-        {
-          turnDrive(turn_spd, 0);
-        }
-        else
-        {
-          turnDrive(turn_spd, -front_error /4);
-        }
+
+        turnDrive(turn_value, move_value);
+        
       }
+    }else{
+      turnDrive(0, 0);
     }
-    else
-    {
-      turnDrive(10, 0);
-    }
+    Brain.Screen.setCursor(3, 1);
+    Brain.Screen.print("|DONE|");
 
-    vex::this_thread::sleep_for(50);
+    vex::this_thread::sleep_for(20);
+    Brain.Screen.clearScreen();
   }
 }
 
--- LAB3/src/robot-config.cpp
+++ LAB3/src/robot-config.cpp
@@ -11,14 +11,14 @@ brain  Brain;
 motor leftDriveMotor = motor(PORT1, ratio18_1, true);
 motor rightDriveMotor = motor(PORT10, ratio18_1, false);
 /*vex-vision-config:begin*/
-signature Vision__SIG_1 = signature (1, 7473, 9063, 8268, -745, -309, -527, 3.8, 0);
+signature Vision__SIG_1 = signature (1, 7473, 9063, 8268, -745, -309, -527, 4.8, 0);
 signature Vision__SIG_2 = signature (2, 0, 0, 0, 0, 0, 0, 3, 0);
 signature Vision__SIG_3 = signature (3, 0, 0, 0, 0, 0, 0, 3, 0);
 signature Vision__SIG_4 = signature (4, 0, 0, 0, 0, 0, 0, 3, 0);
 signature Vision__SIG_5 = signature (5, 0, 0, 0, 0, 0, 0, 3, 0);
 signature Vision__SIG_6 = signature (6, 0, 0, 0, 0, 0, 0, 3, 0);
 signature Vision__SIG_7 = signature (7, 0, 0, 0, 0, 0, 0, 3, 0);
-vision Vision = vision (PORT5, 50, Vision__SIG_1, Vision__SIG_2, Vision__SIG_3, Vision__SIG_4, Vision__SIG_5, Vision__SIG_6, Vision__SIG_7);
+vision Vision = vision (PORT5, 80, Vision__SIG_1, Vision__SIG_2, Vision__SIG_3, Vision__SIG_4, Vision__SIG_5, Vision__SIG_6, Vision__SIG_7);
 /*vex-vision-config:end*/
 
 // VEXcode generated functions
